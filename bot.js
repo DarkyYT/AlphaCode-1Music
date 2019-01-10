ab39b7e225d73c5cbf3c217532569bb79d2b288a
DarkyYT <46546451+DarkyYT@users.noreply.github.com>
2019-01-10 18:49:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,10 +20,12 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "!";
+const prefix = process.env.P;
 /////////////////////////
 ////////////////////////
-
+client.on('ready', function(){
+	 client.user.setActivity(process.env.G);
+ });
 client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
