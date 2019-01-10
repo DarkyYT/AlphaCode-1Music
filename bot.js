74ce73e0c5b4fbfa918bea7b95a25a258d453ff9
DarkyYT <46546451+DarkyYT@users.noreply.github.com>
2019-01-10 20:36:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,6 +48,7 @@ client.on('message', async msg =>{
 /////////////////////////
 ////////////////////////
 //////////////////////
+
 client.on('message', async msg =>{
 	if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
@@ -56,7 +57,25 @@ client.on('message', async msg =>{
 
 	let command = msg.content.toLowerCase().split(" ")[0];
 	command = command.slice(prefix.length)
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
 
+  if (command === 'join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
+	
     if(command === `avatar`){
 	if(msg.channel.type === 'dm') return msg.channel.send("Nope Nope!! u can't use avatar command in DMs (:")
         let mentions = msg.mentions.members.first()
